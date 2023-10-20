ef5ff7188d608b2aa44e2dd930b310b30b9244bd
Liza <y.koliechkina@gmail.com>
2023-10-20 19:27:25 +0200

    feat: clusters create and update, cluster list by countries

--- api/__init__.py
+++ api/__init__.py
@@ -1,2 +1,19 @@
-response_500 = {'description': 'Failed to retrieve data from the database', 'model': str}
-response_404 = {'description': 'No data were found for given parameters', 'model': str}
+from pydantic import BaseModel
+
+
+class HTTPError(BaseModel):
+    detail: str
+
+    class Config:
+        json_schema_extra = {
+            'example': {'detail': 'Failed to process the request.'},
+        }
+
+
+response_400 = {'description': 'Invalid input provided.', 'model': HTTPError}
+response_404 = {'description': 'No data found for provided parameters.', 'model': HTTPError}
+response_409 = {
+    'description': 'Failed to create a new object due to a duplicate entry in the database.',
+    'model': HTTPError
+}
+response_500 = {'description': 'Failed to retrieve data from the database.', 'model': HTTPError}
--- schemas/cluster.py
+++ schemas/cluster.py
@@ -1,16 +1,51 @@
-from typing import Optional
-
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 
-class Cluster(BaseModel):
-    name: str = Field(..., title='Product internal name', max_length=100)
-    description: Optional[str] = Field(None, title='Description', max_length=300)
+class CountryClusters(BaseModel):
+    data: dict[str, list[str]]
+
+
+class ClusterBase(BaseModel):
+    name: str = Field(..., title='Cluster unique name', max_length=50)
+    description: str = Field(None, max_length=300)
+
+
+class ClusterInput(ClusterBase):
     division: str = Field(..., max_length=50)
-    nof_products: int = Field(..., title='Number of products in the cluster')
-    sold_quantity: int = Field(..., title='Quantity of products sold')
+    previous_sold_quantity: int = Field(
+        default=0,
+        ge=0,
+        title='Quantity of products sold for previous period'
+    )
+
+
+class ClusterUpdateInput(ClusterBase):
+    division: str = Field(None, max_length=50)
+    previous_sold_quantity: int = Field(
+        None,
+        ge=0,
+        title='Quantity of products sold for previous period'
+    )
+
+
+class Cluster(ClusterBase):
+    division: str = Field(..., max_length=50)
+    previous_sold_quantity: int = Field(
+        ...,
+        ge=0,
+        title='Quantity of products sold for previous period'
+    )
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class ClusterSalesData(ClusterBase):
+    division: str = Field(..., max_length=50)
+    nof_products: int = Field(..., ge=0, title='Number of products in the cluster')
+    sold_quantity: int = Field(..., ge=0, title='Quantity of products sold')
     net_sales: float = Field(
         ...,
+        ge=0.0,
         title='Net sales',
         description='Net value of all units sold'
     )
